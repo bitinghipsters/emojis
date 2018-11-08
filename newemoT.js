5af94c74a7d8c031aa935ec10b7be19e67ed15d0
bitinghipsters <44856490+bitinghipsters@users.noreply.github.com>
2018-11-08 02:28:35 -0500

    Create newemoT.js

---
+++ newemoT.js
@@ -0,0 +1,92 @@
+var newemoT = '\
+<div class="emoji-table">\
+<div class="ec-menu"></div>\
+<div class="emoji-wrap">\
+<div class="emoji-cat">\
+<input type="radio" id="people" name="emojitab" checked>\
+<label for="people"><img src="https://github.com/bitinghipsters/emojis/blob/master/peoplesmilies.png?raw=true" title="people & smilies"></label>\
+<div class="emoji-list">\
+<div class="el-cat">people & smilies</div>\
+<div class="el-list">\
+</div>\
+</div>\
+</div>\
+</div>\
+<div class="emoji-wrap">\
+<div class="emoji-cat">\
+<input type="radio" id="nature" name="emojitab">\
+<label for="nature"><img src="https://github.com/bitinghipsters/emojis/blob/master/nature.png?raw=true" title="animals & nature"></label>\
+<div class="emoji-list">\
+<div class="el-cat">animals & nature</div>\
+<div class="el-list">\
+</div>\
+</div>\
+</div>\
+</div>\
+<div class="emoji-wrap">\
+<div class="emoji-cat">\
+<input type="radio" id="food" name="emojitab">\
+<label for="food"><img src="https://github.com/bitinghipsters/emojis/blob/master/food.png?raw=true" title="food & drink"></label>\
+<div class="emoji-list">\
+<div class="el-cat">food & drink</div>\
+<div class="el-list">\
+</div>\
+</div>\
+</div>\
+</div>\
+<div class="emoji-wrap">\
+<div class="emoji-cat">\
+<input type="radio" id="activity" name="emojitab">\
+<label for="activity"><img src="https://github.com/bitinghipsters/emojis/blob/master/activity.png?raw=true" title="activities & sports"></label>\
+<div class="emoji-list">\
+<div class="el-cat">activities & sports</div>\
+<div class="el-list">\
+</div>\
+</div>\
+</div>\
+</div>\
+<div class="emoji-wrap">\
+<div class="emoji-cat">\
+<input type="radio" id="travel" name="emojitab">\
+<label for="travel"><img src="https://github.com/bitinghipsters/emojis/blob/master/travel.png?raw=true" title="travel & places"></label>\
+<div class="emoji-list">\
+<div class="el-cat">travel & places</div>\
+<div class="el-list">\
+</div>\
+</div>\
+</div>\
+</div>\
+<div class="emoji-wrap">\
+<div class="emoji-cat">\
+<input type="radio" id="objects" name="emojitab">\
+<label for="objects"><img src="https://github.com/bitinghipsters/emojis/blob/master/objects.png?raw=true" title="objects"></label>\
+<div class="emoji-list">\
+<div class="el-cat">objects</div>\
+<div class="el-list">\
+</div>\
+</div>\
+</div>\
+</div>\
+<div class="emoji-wrap">\
+<div class="emoji-cat">\
+<input type="radio" id="symbols" name="emojitab">\
+<label for="symbols"><img src="https://github.com/bitinghipsters/emojis/blob/master/symbols.png?raw=true" title="symbols"></label>\
+<div class="emoji-list">\
+<div class="el-cat">symbols</div>\
+<div class="el-list">\
+</div>\
+</div>\
+</div>\
+</div>\
+<div class="emoji-wrap">\
+<div class="emoji-cat">\
+<input type="radio" id="flags" name="emojitab">\
+<label for="flags"><img src="https://github.com/bitinghipsters/emojis/blob/master/flags.png?raw=true" title="flags"></label>\
+<div class="emoji-list">\
+<div class="el-cat">flags</div>\
+<div class="el-list">\
+</div>\
+</div>\
+</div>\
+</div>\
+</div>'
